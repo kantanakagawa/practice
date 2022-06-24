7fef5e54e24d1624c9ec3032aa6227b4456bb3ae
kantanakagawa <kan0401naka@gmail.com>
2022-06-24 15:37:37 +0900

    update

--- homework_B_5.py
+++ homework_B_5.py
@@ -1,28 +1,38 @@
 numbers = input(("データを入力してください(スペース区切り) > ")).split()
 
-sum_number = 0
-for r in range(0, len(numbers)):
-    number = int(numbers[r])
-    sum_number += number
 
-print(f"合計値: {sum_number}")
-
-
-maxnumber = numbers[0]
-
-for i in range(0, len(numbers)):
-    if numbers[i] > maxnumber:
-        maxnumber = numbers[i]
-
-print(f"最大値：{maxnumber}")
-
-minnumber = numbers[0]
-for i in range(0, len(numbers)):
-    if numbers[i] < minnumber:
-        minnumber = numbers[i]
-
-print(f"最小値：{minnumber}")
+def sum():
+    sum_number = 0
+    for r in range(0, len(numbers)):
+        number = int(numbers[r])
+        sum_number += number
+        return sum_number
 
 
-average = sum_number / len(numbers)
-print(f"平均値: {average}")
+def max():
+    maxnumber = numbers[0]
+    for i in range(0, len(numbers)):
+        if numbers[i] > maxnumber:
+            maxnumber = numbers[i]
+        return maxnumber
+
+
+def min():
+    minnumber = numbers[0]
+    for i in range(0, len(numbers)):
+        if numbers[i] < minnumber:
+            minnumber = numbers[i]
+        return minnumber
+
+
+def average():
+
+    average = int(sum() / len(numbers))
+    return average
+
+
+if __name__ == "__main__":
+    print(f"合計値： {sum()}")
+    print(f"最大値： {max()}")
+    print(f"最小値： {min()}")
+    print(f"平均値： {average()}")
